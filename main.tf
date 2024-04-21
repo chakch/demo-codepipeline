25bda28ceb95dde9e4d50e571bd81dcaa4b091fa
chakch <chakch007@gmail.com>
2024-04-21 16:33:48 +0200

    update

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
 terraform {
     backend "s3" {}
 }
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.46.0"
+    }
+  }
+}
+
 variable "prefix"{
 
 }
